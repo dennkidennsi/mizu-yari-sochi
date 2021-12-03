2c8b48015ce829be2bfbfd8031f70a2929fa9c56
dennkidennsi <95457444+dennkidennsi@users.noreply.github.com>
2021-12-03 11:59:27 +0900

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="水やり装置">
+    <h1>{{ msg }}</h1>
+    <img src = "http://192.168.0.151:8081">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
